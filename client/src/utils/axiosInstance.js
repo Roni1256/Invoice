876a2f90f874d7e3dda4c21427df68b4e580a5f9
Roniwilliams <ronideni1220@gmail.com>
2025-11-03 12:14:20 +0530

    added ports and origin

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-const BASE_URL = import.meta.env.VITE_NODE_ENV === 'development' ? 'http://localhost:5000/api' : ''
+const BASE_URL = import.meta.env.VITE_NODE_ENV === 'development' ? 'http://localhost:5000/api' : 'https://smart-billing-server.vercel.app/api'
 
 export const axiosInstance = axios.create({
     baseURL: BASE_URL,
